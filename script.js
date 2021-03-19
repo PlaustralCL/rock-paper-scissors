b44c6e5f1010a1ca0ada3a2394c26509c3454656
PlaustralCL <plaustralcl@gmail.com>
2021-03-18 21:40:30 -0400

    Add capabililty to play one round w/o player input
    Able to play one round with predefined playerSelection. No player input.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-console.log(computerPlay());
+
+
+  let playerSelection = "scissors";
+  let computerSelection = computerPlay();
+
+  console.log("You chose " + playerSelection);
+  console.log("The computer chose " + computerSelection);
+
+  console.log(playRound(playerSelection, computerSelection));
+
+
+
+
 
 function computerPlay() {
   let computerSelection = getRandomIntInclusive(1, 3);
@@ -20,7 +32,47 @@ function computerPlay() {
 
 function getRandomIntInclusive(min, max) { 
   // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); 
 }
+
+function getPlayerInput() {
+  let selection = prompt("Choose 'Rock', 'Paper' or 'Scissors'");
+  return selection.toLowerCase;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (playerSelection === "rock") {
+      switch (computerSelection) {
+        case "paper":
+          return "Paper beats rock. You loose";
+          break;
+        case "scissors":
+          return "Rock beats scissors. You win!";
+          break;
+        default:
+          return "default"
+      }
+    } else if (playerSelection === "paper") {
+      switch (computerSelection) {
+        case "rock":
+          return "Paper beats rock. You win!";
+          break;
+        case "scissors":
+          return "Scissors beat paper. You loose."
+          break;
+        default:
+          return "default"
+      }
+    } else if (playerSelection === "scissors") {
+      switch (computerSelection) {
+        case "rock":
+          return "Rock beat scissors. You loose";
+          break;
+        case "paper":
+          return "Scissors beat paper. You win!"
+      }
+    }
+}
+
