d82f99c57a0339a5e5362db6c03440b9b2ad74be
PlaustralCL <plaustralcl@gmail.com>
2021-03-18 20:34:36 -0400

    create computerPlay function
    Created a function to randomly select rock, paper, or scissors. Used
    another function to generate a random interger between 1 and 3.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+console.log(computerPlay());
+
+function computerPlay() {
+  let computerSelection = getRandomIntInclusive(1, 3);
+  switch (computerSelection) {
+    case 1:
+      computerSelection = "rock";
+      break;
+    case 2:
+      computerSelection = "paper";
+      break;
+    case 3:
+      computerSelection = "scissors";
+      break;
+    default:
+      computerSelection = "default";
+  }
+  return computerSelection;
+}
+
+function getRandomIntInclusive(min, max) { 
+  // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}
